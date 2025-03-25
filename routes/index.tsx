11c4f97fbc47a524a4efc5388eda65101ff595d0
panpann2305 <s4118650@student.rmit.edu.au>
2025-03-25 11:39:07 +1100

    hope it works

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,15 +10,19 @@ export const handler: Handlers<Post[]> = {
 
 export default function BlogIndexPage(props: PageProps<Post[]>) {
   const posts = props.data;
+
   return (
-    <main class="max-w-screen-md px-4 pt-16 mx-auto">
-      <h1 class="text-5xl font-bold">Blog</h1>
-      <div class="mt-8">
-        {posts.map((post) => <PostCard post={post} />)}
+    <main className="max-w-screen-md px-4 pt-16 mx-auto">
+      <h1 className="text-5xl font-bold">Blog</h1>
+      <div className="mt-8">
+        {posts.map((post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
       </div>
     </main>
   );
 }
+
 
 function PostCard(props: { post: Post }) {
   const { post } = props;
